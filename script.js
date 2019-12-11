4e0f0f4bce30340308584ba6d482851d273dde1c
Chengyu Ko <kokevin678@gmail.com>
2019-12-10 16:54:59 -0800

    add row and col

---
+++ script.js
@@ -0,0 +1,25 @@
+//add row and col to container
+var row = $("<div class='row'>");
+// var col1 = $("<div><p class='hour'></p>");
+var col1 = $("<div class='col-sm-2'><p class='hour'></p>");
+
+// var col2 = $("<div><textarea class='description'></textarea>");
+var col2 = $("<div class='col-sm-8'><textarea class='description'></textarea>");
+
+// var col3 = $("<div><button class='saveBtn'></button>");
+var col3 = $("<div class='col-sm-2'><button class='saveBtn'></button>");
+
+
+row.append(col1);
+row.append(col2);
+row.append(col3);
+
+$(".container").append(row);
+
+
+//add more rows
+//get local storage
+//use moment.js to set time
+//
+
+
